bde2426a54ba99c058967f93d8650ab85f5c3556
shs <orolsyeo@gmail.com>
2021-11-18 17:46:00 +0900

    h2, mysql databse un setup and postgresql setup !

--- src/main/java/me/sseob/demowebapplication/postgres/PostgresRunner.java
+++ src/main/java/me/sseob/demowebapplication/postgres/PostgresRunner.java
@@ -1,4 +1,4 @@
-package me.sseob.demowebapplication.mysql;
+package me.sseob.demowebapplication.postgres;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -8,29 +8,40 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.Statement;
 
 @Component
-public class MysqlRunner implements ApplicationRunner {
+public class PostgresRunner implements ApplicationRunner {
 
 	private final DataSource dataSource;
 	private final JdbcTemplate jdbcTemplate;
 
-	public MysqlRunner(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+	@Autowired
+	public PostgresRunner(DataSource dataSource, JdbcTemplate jdbcTemplate) {
 		this.dataSource = dataSource;
 		this.jdbcTemplate = jdbcTemplate;
 	}
-
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		
 		//try catch with resource
 		try(Connection connection = dataSource.getConnection()) {
+
+			DatabaseMetaData metaData = connection.getMetaData();
+			System.out.println("url: " + metaData.getURL());
+			System.out.println("driver name: " + metaData.getDriverName());
+			System.out.println("user name: " + metaData.getUserName());
+
 			Statement statement = connection.createStatement();
-			String sql = "CREATE TABLE USER(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (ID));";
+			
+			String delete = "drop table account;";
+			statement.execute(delete);
+			String sql = "CREATE TABLE ACCOUNT(ID INTEGER NOT NULL, name VARCHAR(255), PRIMARY KEY (ID));";
 			statement.executeUpdate(sql);
 		}
 
-		jdbcTemplate.execute("INSERT INTO USER VALUES (1,'sseob')");
+		jdbcTemplate.execute("INSERT INTO ACCOUNT VALUES (1,'sseob')");
 	}
 }
