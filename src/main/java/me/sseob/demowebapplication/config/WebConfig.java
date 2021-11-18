d731cb647205c3e58d0346bc39133dcc6df71a76
shs <orolsyeo@gmail.com>
2021-11-18 13:48:02 +0900

    cors setup !

--- src/main/java/me/sseob/demowebapplication/config/WebConfig.java
+++ src/main/java/me/sseob/demowebapplication/config/WebConfig.java
@@ -16,8 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
 	}
 
 	/*
-		좀 더 광범위한 mapping을 허용하려면, 
-		@CorsOrigin annotation을 마구마구 쓰는 대신에 좀 더 간단하면서도 광범위하게 설정하자 ㅎㅎㅎ
+		좀 더 Global하게 Cross origin mapping을 허용하려면, 
+		@CorsOrigin annotation을 마구마구 쓰는 대신에 기능을 확장하여 간단하게 설정하자 ㅎㅎㅎ
 	 */
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
