7702dadf1c6070de9120c803cf41e53e1a1b8d2d
hyun-sseob <orolsyeo@gmail.com>
2021-11-30 20:54:13 +0900

    security config

--- src/main/java/me/sseob/demowebapplication/config/WebSecurityConfig.java
+++ src/main/java/me/sseob/demowebapplication/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+// conditionalxxx annotation으로 자동설정되던 부분들이 WebSecurityConfigurerAdapter class를 상속받음에 따라 자동설정 되지 않는다 !
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
